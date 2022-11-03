3b6031e3b68c8ecc4ddb6d9121064e0961f1f4d1
Mark Campbell <markcampbell114@gmail.com>
2022-11-03 16:30:37 +0000

    E2E - Filtering Test

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,8 @@
 export const filterByName = (actors, string) =>
   actors.filter((a) => a.name.toLowerCase().search(string) !== -1);
+
+export const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+export const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
